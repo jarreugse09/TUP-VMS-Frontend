3d9a580bbd5ab73d08cce6e3fd430c47fe2ffc06
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-14 18:00:43 +0800

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: BASE_URL,
+  withCredentials: true, // if you want to send cookies
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-// Add token to requests
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+
 export default api;
