735f93e263a60fa14db610c84ac5abe0e2689654
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-14 18:19:17 +0800

    authservice

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,17 +1,17 @@
 import api from "./api";
 
 export const login = async (email: string, password: string) => {
-  const response = await api.post("/auth/login", { email, password });
+  const response = await api.post("/api/auth/login", { email, password });
   return response.data;
 };
 
 
 export const register = async (userData: any) => {
-  const response = await api.post("/auth/register", userData);
+  const response = await api.post("/api/auth/register", userData);
   return response.data;
 };
 
 export const logout = async () => {
-  const response = await api.post("/auth/logout");
+  const response = await api.post("/api/auth/logout");
   return response.data;
 };
