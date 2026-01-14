7c200e24424dabc74c3dd1cb2a5341c6fd12be84
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-14 18:03:14 +0800

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
