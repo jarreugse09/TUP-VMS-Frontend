c20c8f4c093be7001a4bc73247f71d2176c9d54d
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-18 16:49:28 +0800

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL, // should include /api
+  baseURL: import.meta.env.VITE_API_URL, // should include /api
   withCredentials: true,
 });
 
